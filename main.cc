3569d9e1d9284f335223f8d7d8aceda9c0d52430
Maggi Sledjeski <sledjeski_ma@gsc_218_8.mercer.edu>
2019-04-23 10:53:20 -0400

    fixed the clipping plane

--- main.cc
+++ main.cc
@@ -43,7 +43,7 @@ void reshape (int w, int h)
 	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
 	glMatrixMode (GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(40.0, (GLfloat) w/(GLfloat) h, 1.0, 40.0);
+	gluPerspective(40.0, (GLfloat) w/(GLfloat) h, 1.0, 60.0);
 	glMatrixMode(GL_MODELVIEW);
 }
 
